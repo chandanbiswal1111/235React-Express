f8d0d3dc5895bb3cc39d88b23a3c6cf2c301b985
chandanbiswal1111 <153886776+chandanbiswal1111@users.noreply.github.com>
2024-01-25 10:37:38 +0530

    Add files via upload

---
+++ ReactDay-3Assignment/TodoList.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+
+const ToDoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+  const handleAddTask = () => {
+    if (newTask.trim() !== '') {
+      setTasks([...tasks, newTask]);
+      setNewTask('');
+    }
+  };
+
+  const handleDeleteTask = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <h2>To-Do List</h2>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => handleDeleteTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button onClick={handleAddTask}>Add Task</button>
+      </div>
+    </div>
+  );
+};
+
+export default ToDoList;
