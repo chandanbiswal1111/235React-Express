b4f571d09d7b4a4b16390752bf56d7d71d74abb0
chandanbiswal1111 <153886776+chandanbiswal1111@users.noreply.github.com>
2024-01-29 22:05:23 +0530

    Add files via upload

---
+++ ReactDay-5 Assignment/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+import Login from './Components/Login';
+import ProtectedRoute from './Components/ProtectedRoute';
+import ProductData from './Components/ProductData';
+
+
+const routing = (
+  <Router>
+    <div style={{ textAlign: "center", padding: "20px", backgroundColor: "cyan", marginBottom: "10px"}}>
+      <Link to='/'>Home</Link> |
+      <Link to='/ProdData'>Product</Link> |
+      <Link to='/login'>Login</Link>
+    </div>
+    <Routes>
+      <Route path='/' element={<App />} ></Route>
+
+      <Route path='/ProdData' element={
+        <ProtectedRoute returnUrl='/ProdData'>
+          <ProductData/>
+        </ProtectedRoute>
+      } ></Route>
+
+      <Route path='/login' element={<Login />} ></Route>
+    </Routes>
+
+  </Router>
+);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {routing}
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
